49d360e5ed54b4e3339d61fb112e1d5b01b821e8
KritikaChhetri <kritikachhetri12345@gmail.com>
2026-09-15 17:29:17 +0530

    Complete Sprint 11 backend setup with Node and Express

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+const testRoutes = require("./routes/testRoutes");
+
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Campus Connect Backend is running");
-});
+// Test Route
+app.use("/api/test", testRoutes);
 
 module.exports = app;
---
+++ server/routes/testRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Campus Connect backend server is running successfully.",
+  });
+});
+
+module.exports = router;
